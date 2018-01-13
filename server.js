d112a055311616c3616e3987fee60e847c1d0eab
Glitch (valley-opera) <none>
2018-01-13 05:45:29 +0000

    🚓👆 Checkpoint server.js:603188/144

--- server.js
+++ server.js
@@ -17,24 +17,21 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+  console.log("index.html");
+});
+/*
+app.get("/:lol", function (request, response) {
+  
+  var amIaNum = Number(request.params.lol);
+  
+  response.json({
+      "unix": lol, 
+      "natural": lol
+  });
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+*/
 
 
 // listen for requests :)
