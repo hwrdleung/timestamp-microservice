68f26ba7fe958ceaca0c95e02b7bc2a666b169ea
Glitch (valley-opera) <none>
2018-01-13 02:26:14 +0000

    👨🐒 Checkpoint server.js:603188/295

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-var express = require('express');
+var express = require("express");
+var http = require("http");
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.listen(port, function(){
+  console.log("Connected to server on port " + port);
+});
